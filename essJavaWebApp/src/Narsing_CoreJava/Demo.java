50e6b861544f40b08f8b4183e0852c47b0a4961c
pudarisrinivas <pudarisrinivas@gmail.com>
2015-08-22 17:45:35 -0500

    Narsing Folder

---
+++ essJavaWebApp/src/Narsing_CoreJava/Demo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package Narsing_CoreJava;
+
+/**
+ * @author SCnu
+ *
+ */
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello");
+	}
+
+}
